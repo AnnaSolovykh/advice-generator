3f54ebeb27c9d536638ef0dc5f6948a8a1b3002f
AnnaSolovykh <ms.teachy.solo@gmail.com>
2022-11-29 13:25:33 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
 
+
+
 function App() {
+
+  const [advice, setAdvice] = useState("Feeling bored?");
+
+  const getAdvice = async() =>{
+    const response = await fetch("http://www.boredapi.com/api/activity/");
+    const data = await response.json();
+    setAdvice(data.activity)
+  };
+
+  useEffect( () => {
+    getAdvice() 
+  }, [] )
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{advice}</h1>
+      <button onClick={ getAdvice }>Get some advice!</button>
     </div>
   );
 }
